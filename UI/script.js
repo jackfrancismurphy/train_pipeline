3178e37eb3ab27f6d79f73876e9da622de6199c0
Jack Murphy <92627635+jackfrancismurphy@users.noreply.github.com>
2025-09-17 10:54:39 +0100

    Update script.js again
    trying to trigger rebuild

--- UI/script.js
+++ UI/script.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Function to fetch data from API
     async function fetchLatestData() {
         try {
-            const response = await fetch('http://34.169.114.91:8080/api/latest');
+            const response = await fetch('http://34.169.114.91:8080/api/latest'); // Google external IP address
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
